30c94613d78505e1ae6fd19f050fe76291f2fffc
OliviaChu <c2232430@gmail.com>
2020-05-17 18:10:51 +0800

    Replace mainwindow.h

--- mainwindow.h
+++ mainwindow.h
@@ -2,21 +2,79 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
-
-namespace Ui {
-class MainWindow;
-}
+#include <QLabel>
+#include <QTextEdit>
+#include <QPushButton>
+#include <QWidget>
+#include <iostream>
+#include <QTime>
+#include <QLineEdit>
+#include <QPushButton>
+#include <QPainter>
+#include <QMessageBox>
+#include <QDebug>
+#include <QKeyEvent>
+#include <QPixmap>
+#include <QMenuBar>
+#include <QMenu>
+#include "Game.h"
+#include <QThread>
+#include <QMutex>
+#include <QMutexLocker>
 
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
 
 public:
-    explicit MainWindow(QWidget *parent = nullptr);
+    MainWindow(QWidget *parent = 0);
+    void paintEvent(QPaintEvent *);
+    void keyPressEvent(QKeyEvent *e);
+    void mousePressEvent(QMouseEvent *event);
     ~MainWindow();
-
+    friend class Game;
 private:
-    Ui::MainWindow *ui;
+    QPixmap wallPixmap;
+    QPixmap aimPixmap;
+    QPixmap manPixmap;
+    QPixmap manOnAimPixmap;
+    QPixmap boxPixmap;
+    QPixmap boxOnAimPixmap;
+    QPixmap goldPixmap;
+    QPixmap YellowwallPixmap;
+    QPixmap BombPixmap;
+    QLabel *roundLabel;
+    QLabel *MaxPointLabel;
+    QLabel *PointLabel;
+    QLabel *stepLabel;
+    QPushButton *nextBtn;
+    QPushButton *restartBtn;
+    QPushButton *preBtn;
+    QPushButton *goBackBtn;
+    QMenu *menu;
+    QMutex mutex;
+    int highpoint;
+    int nowpoint;
+    int round;
+    int cnt;
+    int timerId;
+    bool TouchYellow;
+    vector<int> showAns;
+    vector<int>::iterator showAnsIt;
+    Game *game;
+    void checkGame();
+    bool loadGame();
+    bool loadGame(QString filename, QString);
+    void nextStep();
+    void preStep();
+private slots:
+    void onNextBtnClicked();
+    void onRestartBtnClicked();
+    void onPreBtnClicked();
+    void onActionClicked();
+    void onGoBackBtnClicked();
+    void onActionLeaveGame();
+    void onNewGame();
 };
 
 #endif // MAINWINDOW_H
